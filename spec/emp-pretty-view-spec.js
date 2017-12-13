61f741086618c1f9d0bba3b0b2d4b662d2eef54a
Ju.chao <cqjcrom@gmail.com>
2017-12-13 14:54:36 +0800

    do initial .

--- spec/emp-pretty-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import EmpPrettyView from '../lib/emp-pretty-view';
-
-describe('EmpPrettyView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
